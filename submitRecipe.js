d0e4f2b18847f8dffea39f1fd54b1dd9282b334f
ATecha <atecha@uw.edu>
2019-04-19 20:02:48 -0700

    form page ready for prettification
    form itself isn't fully functional yet

--- submitRecipe.js
+++ submitRecipe.js
@@ -1,4 +1,5 @@
 ///var recipeForm = $("#recipeForm");
+/*
 $(function() { //shorthand document.ready function
     $('#recipeForm').on('submit', function(e) { //use on if jQuery 1.7+
         alert("external js");
@@ -7,9 +8,10 @@ $(function() { //shorthand document.ready function
         console.log(data); //use the console for debugging, F12 in Chrome, not alerts
     });
 });
+*/
 
 
-alert("Page loaded");
+//alert("Page loaded");
 
 var config = {
   apiKey: "AIzaSyACvKZECcLtuQ8D3gKBuWCrqfQ4Varlg0U",
@@ -28,9 +30,17 @@ firebase.initializeApp(config);
 //reference recommendations object in Firebase db
 var recipes = firebase.database().ref("recipes");
 
-//save new recipe to db, using input from form
-var submitRecipe = function() {}
+//listen for form submit
+document.getElementById("recipeForm").addEventListener("submit", submitForm);
 
+
+//save new recipe to db, using input from form
+//var submitRecipe = function() {}
+
+function submitForm(e){
+  e.preventDefault();
+
+/*
   var name = $("#recipeName").val();
   var author = $("#recipeAuthor").val();
   var iQty = $("#ingredientQty").val();
@@ -38,23 +48,39 @@ var submitRecipe = function() {}
   var iName = $("#ingredientName").val();
   var steps = $("#steps").val();
   var mealType = $("#mealType").val();
+  */
   //also need need for check boxes
 
-  recipes.push({
-    "name": name,
-    "author": author,
-    "iName": iName,
-    "iQty": iQty,
-    "iMeasure": iMeasure,
-    "steps": steps,
-    "mealType": mealType
+  var name = getInputVal("recipeName");
+  var author = getInputVal("recipeAuthor");
+  var iQty = getInputVal("ingredientQty");
+  var iMeasure = getInputVal("ingredientMeasurement");
+  var iName = getInputVal("ingredientName");
+  var steps = getInputVal("steps");
+  var mealType = getInputVal("mealType");
+
+  //save to database
+  saveRecipe(name, author, iQty, iMeasure, iName, steps, mealType);
+}
+
+function saveRecipe(name, author, iQty, iMeasure, iName, steps, mealType){
+  var newRecipeRef = recipes.push();
+  newRecipeRef.set({
+    name: name,
+    author: author,
+    iName: iName,
+    iQty: iQty,
+    Measure: iMeasure,
+    steps: steps,
+    mealType: mealType
   });
-};
+}
 
-
+/*
 $(window).load(function (){
 
   $("#recipeForm").submit(submitRecipe);
   alert("Recipe submitted");
   event.preventDefault();
 });
+*/
